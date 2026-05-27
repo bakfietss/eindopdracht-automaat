ce0831645080a7903b6981ec7797f792fd14b9d7
bakfietss <webshophethuisdiertje@gmail.com>
2026-05-27 21:48:29 +0200

    customer model en service stub

---
+++ src/main/java/nl/automaat/api/model/Customer.java
@@ -0,0 +1,27 @@
+package nl.automaat.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+}
---
+++ src/main/java/nl/automaat/api/service/CustomerService.java
@@ -0,0 +1,7 @@
+package nl.automaat.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+}
