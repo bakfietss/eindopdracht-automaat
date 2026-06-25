7c089fa3b7d9fd3514c786047ad72f9e7733e03a
bakfietss <webshophethuisdiertje@gmail.com>
2026-06-25 22:47:13 +0200

    car model basis

---
+++ src/main/java/nl/automaat/api/model/Car.java
@@ -0,0 +1,30 @@
+package nl.automaat.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "license_plate", nullable = false, unique = true, length = 10)
+    private String licensePlate;
+
+    @Column(nullable = false, length = 50)
+    private String brand;
+
+    @Column(nullable = false, length = 50)
+    private String model;
+
+    @Column(name = "build_year", nullable = false)
+    private Integer buildYear;
+}
